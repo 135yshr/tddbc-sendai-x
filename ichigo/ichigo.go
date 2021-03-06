6848ee86cdbf6b8b0dcc885a173eb21eaa09a2c9
135yshr <isago@oreha.dev>
2021-03-06 19:48:20 +0900

    successfully second test

--- ichigo/ichigo.go
+++ ichigo/ichigo.go
@@ -1,10 +1,12 @@
 package ichigo
 
+import "fmt"
+
 type Ichigo struct {
 	Name string
 	Size string
 }
 
 func (c *Ichigo) String() string {
-	return "とちおとめ: L"
+	return fmt.Sprintf("%s: %s", c.Name, c.Size)
 }
